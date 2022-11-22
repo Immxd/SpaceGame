b910edae173eba7a77cc7f59b294b02d296b81c1
Immxd <modmod8883@gmail.com>
2022-11-22 14:34:17 +0700

    003

--- Space_game_2D/Assets/Scripts/PlayerController.cs
+++ Space_game_2D/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D rb;
     private int score;
+    private int Life = 3;
+
+    [SerializeField] private float speed = 4.0f;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text LifeText;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        LifeText.text = "Life : " + Life.ToString();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector2(horizontal * 4, vertical * 4);
+        rb.velocity = new Vector2(horizontal, vertical)*speed;
 
         //print(vertical)
     }
@@ -35,8 +44,18 @@ public class PlayerController : MonoBehaviour
         else if (collision.gameObject.tag == "Ukkabath")
         {
             score = score - 5;
+            Life = Life - 1;
+            Destroy(collision.gameObject);
+            LifeText.text = "Life : " + Life.ToString();
+            if (Life == 0)
+            {
+                LifeText.text = "GameOver";
+            }
+
         }
 
-        print(score);
+        //print(score);
+        scoreText.text = "Score : "+score.ToString();
+
     }
 }
